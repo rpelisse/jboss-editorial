9b0da9371b56f7ad6e55586da3fa2bea4060e954
Romain Pelisse <belaran@gmail.com>
2016-03-07 07:32:11 +0100

    Enhance output

--- post-by-author.sh
+++ post-by-author.sh
@@ -5,5 +5,6 @@ readonly ROSTER_FILE=${ROSTER_FILE:-'roster.txt'}
 for trigram in $(head "${ROSTER_FILE}"  | grep "=" | cut -f1 -d\  )
 do
     nbPost=$(grep -e "${trigram}" "${ROSTER_FILE}" | sed -e '/=/d' | wc -l )
-    echo "${trigram}: ${nbPost}"
+    name=$(grep -e "${trigram}" "${ROSTER_FILE}" | grep -e '=' | sed -e 's/^[^"]*"//' -e 's/".*$//')
+    echo "${name}: ${nbPost}"
 done
