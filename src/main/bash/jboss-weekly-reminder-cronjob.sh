0cbb2406b215c59b35df17c05e8fc9b1d0550fcb
Romain Pelisse <belaran@gmail.com>
2017-04-12 15:30:32 +0200

    remove unused files

--- src/main/bash/jboss-weekly-reminder-cronjob.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-readonly ROSTER_HOME='' # where the clone of the github project lives
-readonly ROSTER=''      # path the roster jar
-readonly SMTP_SERVER='' # SMTP Settings
-readonly SMTP_PORT=''
-
-"${ROSTER_HOME}/src/main/bash/run-scala.sh" "${ROSTER_HOME}/src/main/scala/roster.scala" \
-    -f "${ROSTER_HOME}/roster.txt" -h "${SMTP_SERVER}" -p "${SMTP_PORT}"
